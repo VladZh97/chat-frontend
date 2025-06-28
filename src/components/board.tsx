d091e534ca9f6545a29039928b3a8465216124a0
vlad <zhovklyi.v@gmail.com>
2025-06-28 15:39:36 +0200

    update: scroll area

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,7 +1,7 @@
 const Board = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="grow p-2">
-      <div className="h-full rounded-xl bg-neutral-50 shadow-md">{children}</div>
+      <div className="h-full overflow-hidden rounded-xl bg-neutral-50 shadow-md">{children}</div>
     </div>
   );
 };
--- src/pages/home/components/main.tsx
+++ src/pages/home/components/main.tsx
@@ -2,16 +2,19 @@ import Board from '@/components/board';
 import Stats from './stats';
 import TotalChatsChart from './total-chats-chart';
 import ChatbotsList from './chatbots-list';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const Main = () => {
   return (
     <Board>
-      <div className="px-8 py-6 text-2xl font-medium text-neutral-900">Dashboard</div>
-      <div className="px-8 pb-8">
-        <Stats />
-        <TotalChatsChart />
-        <ChatbotsList />
-      </div>
+      <ScrollArea className="h-full">
+        <div className="px-8 py-6 text-2xl font-medium text-neutral-900">Dashboard</div>
+        <div className="px-8 pb-8">
+          <Stats />
+          <TotalChatsChart />
+          <ChatbotsList />
+        </div>
+      </ScrollArea>
     </Board>
   );
 };
