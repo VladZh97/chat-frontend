50464c7f61c0aa9c8747e894d79a2e1bc6a88e3e
vlad <zhovklyi.v@gmail.com>
2025-07-19 17:34:55 +0200

    update: get chatbots count

--- src/pages/usage/components/stats.tsx
+++ src/pages/usage/components/stats.tsx
@@ -1,4 +1,7 @@
+import chatbot from '@/api/chatbot';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useQuery } from '@tanstack/react-query';
 import { Bot, CreditCard, MessagesSquare } from 'lucide-react';
 import { useCallback } from 'preact/hooks';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +11,12 @@ const Stats = () => {
   const handleChangePlan = useCallback(() => {
     navigate('/subscription');
   }, [navigate]);
+
+  const { data: chatbots } = useQuery({
+    queryKey: ['chatbots'],
+    queryFn: () => chatbot.get(),
+  });
+
   return (
     <div className="mb-4 grid grid-cols-3 gap-4">
       <Card>
@@ -22,13 +31,7 @@ const Stats = () => {
           </Button>
         </div>
       </Card>
-      <Card>
-        <div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-900">
-          <Bot className="size-4 text-neutral-500" />
-          Chatbots
-        </div>
-        <span className="text-2xl font-bold text-neutral-900">1/1</span>
-      </Card>
+      <Chatbots />
       <Card>
         <div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-900">
           <MessagesSquare className="size-4 text-neutral-500" />
@@ -44,4 +47,25 @@ export default Stats;
 
 const Card = ({ children }: { children: React.ReactNode }) => {
   return <div className="rounded-xl border border-neutral-200 bg-white p-6 shadow">{children}</div>;
+};
+
+const Chatbots = () => {
+  const { data: chatbots, isLoading } = useQuery({
+    queryKey: ['chatbots'],
+    queryFn: () => chatbot.get(),
+  });
+
+  return (
+    <Card>
+      <div className="mb-2 flex items-center gap-2 text-sm font-medium text-neutral-900">
+        <Bot className="size-4 text-neutral-500" />
+        Chatbots
+      </div>
+      {isLoading ? (
+        <Skeleton className="size-8" />
+      ) : (
+        <span className="text-2xl font-bold text-neutral-900">{chatbots?.length ?? 0}/1</span>
+      )}
+    </Card>
+  );
 };
