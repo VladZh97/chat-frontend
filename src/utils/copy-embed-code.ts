9e3caa961e8cab90c2479ef69545dbd675dacf55
vlad <zhovklyi.v@gmail.com>
2025-08-17 19:34:56 +0200

    update: embed url

--- src/utils/copy-embed-code.ts
+++ src/utils/copy-embed-code.ts
@@ -1,7 +1,7 @@
 import { toast } from 'sonner';
 
 export const copyEmbedCode = async (accountId: string, _id: string) => {
-  const code = `<script src="https://assets.heyway.chat/${accountId}/${_id}/widget.js" async></script>`;
+  const code = `<script async src="https://assets.heyway.chat/${accountId}/${_id}.js"></script>`;
   await navigator.clipboard.writeText(code);
   toast.success('Embed code copied to clipboard');
 };
