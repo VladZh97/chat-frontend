1390a2f40f486bd4d93bb98971804ac710884709
vlad <zhovklyi.v@gmail.com>
2025-08-17 19:36:54 +0200

    update: loading state color

--- src/pages/chatbot/playground/components/widget.tsx
+++ src/pages/chatbot/playground/components/widget.tsx
@@ -189,6 +189,7 @@ const BotMessageLoading = () => {
     accentColor: s.accentColor,
     avatar: s.avatarIcon,
   }));
+  const textOnAccent = useTextOnAccent(accentColor);
   return (
     <div
       className="flex items-end gap-2"
@@ -197,11 +198,20 @@ const BotMessageLoading = () => {
       <div className="size-6 overflow-hidden rounded-full" style={{ backgroundColor: accentColor }}>
         {avatar && <img src={avatar} alt="avatar" className="size-full object-cover" />}
       </div>
-      <p className="py- flex h-11 items-center gap-1 rounded-2xl rounded-bl-none bg-[var(--accent-color)]/10 px-4">
+      <p className="flex h-11 items-center gap-1 rounded-2xl rounded-bl-none bg-[var(--accent-color)]/10 px-4">
         <span className="sr-only">Assistant is typing…</span>
-        <span className="inline-block size-1.5 animate-bounce rounded-full bg-stone-900 [animation-delay:-0.3s]"></span>
-        <span className="inline-block size-1.5 animate-bounce rounded-full bg-stone-900 [animation-delay:-0.15s]"></span>
-        <span className="inline-block size-1.5 animate-bounce rounded-full bg-stone-900"></span>
+        <span
+          style={{ backgroundColor: textOnAccent.color }}
+          className="inline-block size-1.5 animate-bounce rounded-full [animation-delay:-0.3s]"
+        ></span>
+        <span
+          style={{ backgroundColor: textOnAccent.color }}
+          className="inline-block size-1.5 animate-bounce rounded-full [animation-delay:-0.15s]"
+        ></span>
+        <span
+          style={{ backgroundColor: textOnAccent.color }}
+          className="inline-block size-1.5 animate-bounce rounded-full"
+        ></span>
       </p>
     </div>
   );
