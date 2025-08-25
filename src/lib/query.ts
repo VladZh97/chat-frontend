2509d549669cdefc6ae3dfe3ed085b99f59c5b21
vlad <zhovklyi.v@gmail.com>
2025-08-25 18:55:35 +0200

    update: query settings

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -6,9 +6,12 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       staleTime: 5 * 60 * 1000,
       gcTime: 30 * 60 * 1000,
-      refetchOnReconnect: false,
+      refetchOnReconnect: true,
       refetchInterval: false,
       retry: 2,
+      retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 30000),
+      networkMode: 'online',
+      refetchOnMount: 'always',
     },
   },
 });
